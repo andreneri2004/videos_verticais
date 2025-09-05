804cb92786929feed6bcf37e258e2954a9b439d8
andre.neri <andreneri@hotmail.com>
2025-09-05 06:43:46 -0300

    Adicionando Video, e Caption

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,14 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
-
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
-  const FullScreenPlayer({super.key, required this.videoUrl, required this.caption});
+  const FullScreenPlayer({
+    super.key,
+    required this.videoUrl,
+    required this.caption,
+  });
+
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshort) {
+      
+      if (snapshort.connectionState != ConnectionState.done){
+        return const  Center(child: CircularProgressIndicator());
+
+      }
+      return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(controller),
+
+            Positioned(
+              bottom: 50,
+              left: 50,
+              child: _VideoCaption(caption: widget.caption)
+              )
+          ],
+        ),
+        );
+      
+      
+    });
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+
+  final String caption;
+  const _VideoCaption({super.key, required this.caption});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,//60%
+      child: Text(caption, style: titleStyle,),
+    );
   }
 }
